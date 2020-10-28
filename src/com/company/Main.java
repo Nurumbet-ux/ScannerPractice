481bc48487d4602a46259b1666cbabfda62230f6
Nurumbet-ux <saryevnur@gmail.com>
2020-10-28 17:00:28 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double d = scan.nextDouble();
+        int i = scan.nextInt();
+        String s = scan.nextLine();
+
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+
+
+    }
+}
